a1017085e7119fa01655a1ba4adcb9bfba047be0
Azure-Agst <August712@protonmail.com>
2019-08-04 20:28:33 -0400

    Add comments to index.js

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
+// index.js
+// (c) 2019 FSUInnovationHub
+
+// Import Consants
 const path = require('path');
 const express = require('express');
 const app = express();
-var http = require('http');
-var fs = require('fs');
+
+// Express Configuration
 app.set('view engine', 'pug');
 app.set('views');
 app.set('views', path.join(__dirname, './views'));
 app.use(express.static('public'));
 
+// MongoDB Variables
 const MongoClient = require('mongodb').MongoClient
-    var ObjectID = require('mongodb').ObjectID;
-    
-    MongoClient.connect('mongodb+srv://USERNAME:PASSWORD@cluster0-q86na.mongodb.net/test?retryWrites=true&w=majority', (err, db) => {
-        var dbase = db.db("CreatorConnect");
-        if (err) return console.log(err)
-        app.listen(3000, () => {})
 
-        app.get('/CreatorConnect', (req, res) => {
-          dbase.collection('users').find().count((err, results) => {
-            dbase.collection('users').find({}, { projection: {_id:0, name:1, email:1, skills:1}}).toArray( (err, allUsers) => {
-              res.render('prototype', {users: allUsers, totalUsers: results})
-            });
-          });
-        });
-      })
+// Main Connect
+MongoClient.connect('mongodb+srv://USERNAME:PASSWORD@cluster0-q86na.mongodb.net/test?retryWrites=true&w=majority', (err, db) => {
+ if (err) return console.log(err)
+ var dbase = db.db("CreatorConnect");
+
+  app.get('/CreatorConnect', (req, res) => {
+    dbase.collection('users').find().count((err, results) => {
+      dbase.collection('users').find({}, { projection: {_id:0, name:1, email:1, skills:1}}).toArray( (err, allUsers) => {
+        res.render('prototype', {users: allUsers, totalUsers: results})
+      });
+    });
+  });
+
+  app.listen(3000, () => {
+    console.log("Server Started on Port")
+  });
+});
