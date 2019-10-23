539bf50d80cfe39b31320edb7ed67aa0c4505e9b
Orlando Kenny <kenny@cs.fsu.edu>
2019-10-22 22:10:44 -0400

    condensed css

--- webapp/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-const App = ({str}) => (
-  <h1>{str}</h1>
-)
-
-export default App;
--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './CreatorConnect.css';
-import App from './App';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+
 import * as serviceWorker from './serviceWorker';
 import AltUsers from './altUsers';
 
-const string = "Welcome to CreatorConnect!"
-ReactDOM.render(<AltUsers/>, document.getElementById('test'));
+const routing = (
+  <Router>
+      <Route path="/home" component={AltUsers} />
+  </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
